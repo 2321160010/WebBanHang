afd372cf781c6842131a9c2db2f9b3fd219bc0fa
ADMIN <ADMIN@PC233>
2025-05-27 12:43:03 +0700

    Add project files.

---
+++ WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index(int page=1)
+        {
+
+            var pageSize = 6;
+            var dsSanpham = _db.Products.ToList();
+            return View(dsSanpham.Skip((page - 1) * pageSize).Take(pageSize).ToList());
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
